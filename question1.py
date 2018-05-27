d6456f2e7d20b5f28ae41450cb8f0833222f8d7f
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-27 19:16:35 +0100

    added test and empty func to q1

---
+++ question1.py
@@ -0,0 +1,2 @@
+def check(lst,v):
+	pass
--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import pytest
 from question1, question2, question3, question4 import *
 
+# test for q1
+
+def testcheck():
+	lst = [1,2,3,4,5,6,7]
+	v = 3
+	v2 = 8
+	assert check(lst,v) == True
+	assert check(lst,v2) == False
+	
